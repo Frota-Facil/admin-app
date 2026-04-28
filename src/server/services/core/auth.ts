c6d61076a888f46af456345343dae6b1b10e64bf
joão manoel <manoel.joao@escolar.ifrn.edu.br>
2026-04-28 10:15:29 -0300

    fix: change core auth rote

--- src/server/services/core/auth.ts
+++ src/server/services/core/auth.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function auth(cpf: string, password: string) {
-  const { data } = await axios.post(`${process.env.CORE_API_URL}/auth`, {
+  const { data } = await axios.post(`${process.env.CORE_API_URL}/auth/admin`, {
     cpf,
     password,
   });
